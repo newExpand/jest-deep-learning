dfec13bb9b50530962c874b0d704c9eec67203ea
newExpand <magin0auto@gmail.com>
2023-08-06 11:36:26 +0900

    test: 서버 데이터 테스트 코드 통과하기 위한 리액트 코드 작업(서버는 getByRole이 아니라 findByRole처럼 find문을 사용해서 비동기로 찾아줘야 함)

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,5 +1,28 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import ScoopOption from "./ScoopOption";
+import Row from "react-bootstrap/Row";
+
 const Options = ({ optionType }) => {
-    return <div>Options</div>;
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get(`http://localhost:3030/${optionType}`)
+            .then((response) => setItems(response.data))
+            .catch((error) => {
+                // TODO: 에러 처리
+            });
+    }, [optionType]);
+
+    // TODO: null 나중에 토핑옵션으로 교체
+    const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+
+    const optionItems = items.map((item) => (
+        <ItemComponent key={item.name} name={item.name} imagePath={item.imagePath} />
+    ));
+
+    return <Row>{optionItems}</Row>;
 };
 
 export default Options;
--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -2,11 +2,11 @@ import { render, screen } from "@testing-library/react";
 
 import Options from "../Options";
 
-test("각각의 스쿱 옵션의 이미지가 서버에 표시되는지 테스트", () => {
+test("각각의 스쿱 옵션의 이미지가 서버에 표시되는지 테스트", async () => {
     render(<Options optionType="scoops" />);
 
     // 이미지를 찾았는지
-    const scoopImages = screen.getAllByRole("img", { name: /scoop$/i });
+    const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
     expect(scoopImages).toHaveLength(2);
 
     // 이미지 alt 텍스트 확인
