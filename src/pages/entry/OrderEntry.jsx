88297466ba8a65d8b1f733e7a2391e0bce14100a
newExpand <magin0auto@gmail.com>
2023-08-12 13:55:52 +0900

    test: 주문한 스쿱이 없을 때 주문하기 버튼 비활성화에 대한 테스트 코드 작성 feat: 테스트 코드에 따른 버튼 비활성화에 대한 리액트 코드 작성

--- src/pages/entry/OrderEntry.jsx
+++ src/pages/entry/OrderEntry.jsx
@@ -6,13 +6,17 @@ import Options from "./Options";
 const OrderEntry = ({ setOrderPhase }) => {
     const { totals } = useOrderDetails();
 
+    const orderDisabled = totals.scoops === 0;
+
     return (
         <div>
             <h1>당신의 선데이를 주문하세요!</h1>
             <Options optionType="scoops" />
             <Options optionType="toppings" />
             <h2>전체 총액: {formatCurrency(totals.scoops + totals.toppings)}원</h2>
-            <Button onClick={() => setOrderPhase("리뷰")}>선데이 주문하기</Button>
+            <Button disabled={orderDisabled} onClick={() => setOrderPhase("리뷰")}>
+                선데이 주문하기
+            </Button>
         </div>
     );
 };
--- src/pages/entry/tests/OrderEntry.test.jsx
+++ src/pages/entry/tests/OrderEntry.test.jsx
@@ -1,4 +1,5 @@
 import { screen, render, waitFor } from "../../../test-utils/testing-library-util";
+import userEvent from "@testing-library/user-event";
 import OrderEntry from "../OrderEntry";
 import { rest } from "msw";
 import { server } from "../../../mocks/server";
@@ -18,4 +19,21 @@ test("토핑과 스쿱 라우트에 대한 에러핸들링 테스트", async () 
 
         expect(alerts).toHaveLength(2);
     });
+});
+
+test("주문한 스쿱이 없을 때 주문하기 버튼 비활성화 테스트", async () => {
+    const user = userEvent.setup();
+    render(<OrderEntry setOrderPhase={jest.fn()} />);
+    const orderSummaryButton = screen.getByRole("button", { name: "선데이 주문하기" });
+    expect(orderSummaryButton).toBeDisabled();
+
+    const vanillaInput = await screen.findByRole("spinbutton", { name: "Vanilla" });
+    await user.clear(vanillaInput);
+    await user.type(vanillaInput, "1");
+
+    expect(orderSummaryButton).toBeEnabled();
+
+    await user.clear(vanillaInput);
+    await user.type(vanillaInput, "0");
+    expect(orderSummaryButton).toBeDisabled();
 });
